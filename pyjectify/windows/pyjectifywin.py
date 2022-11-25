f9741d5e4e8ca584ff315d24924c6e24713ab82a
Petitoto <Petitoto@users.noreply.github.com>
2022-11-25 23:03:20 +0100

    Add PyJectifyWin

---
+++ pyjectify/windows/pyjectifywin.py
@@ -0,0 +1,17 @@
+from pyjectify.windows.core.process import getpid, ProcessHandle
+from pyjectify.windows.core.pe import PE
+
+from pyjectify.windows.modules.memscan import MemScan
+from pyjectify.windows.modules.inject import Inject
+
+
+def byName(process):
+    return [PyJectifyWin(pid) for pid in getpid(process)]
+
+
+class PyJectifyWin:
+    def __init__(self, pid):
+        self.process = ProcessHandle(pid)
+        
+        self.memscan = MemScan(self.process)
+        self.inject = Inject(self.process)
