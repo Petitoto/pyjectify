c234c804f72574d9ceeff5182188116e36f9e4c2
Petitoto <Petitoto@users.noreply.github.com>
2023-11-02 22:57:45 +0100

    Fix Python GIL issues

--- docs/pyjectify_win/modules/pythonlib.rst
+++ docs/pyjectify_win/modules/pythonlib.rst
@@ -2,6 +2,7 @@
 ================
 
 Classes, exceptions, and methods to run Python code in a remote process.
+Python 3.10 & 3.11 supported. Fix for other versions WIP
 
 .. automodule:: pyjectify.windows.modules.pythonlib
    :members:
--- pyjectify/windows/modules/pythonlib.py
+++ pyjectify/windows/modules/pythonlib.py
@@ -65,6 +65,7 @@ class PythonLib:
     
     def setpythonhome(self, pythonhome: str) -> None:
         """Set the default "home" directory
+        This method calls Py_SetPythonHome
         
         Args:
             pythonhome: the PYTHON_HOME value
@@ -83,6 +84,7 @@ class PythonLib:
     
     def isinitialized(self) -> bool:
         """Check if the Python interpreter is initialized in the target process
+        This method calls Py_IsInitialized
         
         Returns:
             A boolean specifying if the Python interpreter is initialized
@@ -96,34 +98,51 @@ class PythonLib:
     
     
     def initialize(self, initsigs: int = 0) -> None:
-        """Initialize the Python interpreter in the target process
+        """Initialize the Python interpreter in the target process and release the GIL
+        This method calls Py_IsInitialized + Py_InitializeEx + PyEval_SaveThread
         
         Args:
             initsigs: initsigs for Py_InitializeEx
         """
-        py_initialize_ex = self.python_mod.base_addr + self.python_mod.exports['Py_InitializeEx']
-        
-        thread = self._process.start_thread(py_initialize_ex, initsigs)
-        self._process.join_thread(thread)
+        if not self.isinitialized():
+            py_initialize_ex = self.python_mod.base_addr + self.python_mod.exports['Py_InitializeEx']
+            py_eval_save_thread = self.python_mod.base_addr + self.python_mod.exports['PyEval_SaveThread']
+            
+            initialize_thread = self._process.start_thread(py_initialize_ex, initsigs)
+            self._process.join_thread(initialize_thread)
+            
+            savethread_thread = self._process.start_thread(py_eval_save_thread, initsigs)
+            self._process.join_thread(savethread_thread)
     
     
     def exec(self, py_code: str) -> None:
-        """Execute python code in the target process. Python interpreter MUST be initialized.
+        """Execute python code in the target process (acquire and then release the GIL). Python interpreter MUST be initialized.
+        This method calls PyGILState_Ensure + PyRun_SimpleString + PyEval_SaveThread
         
         Args:
             py_code: the python code to run
         """
+        py_gil_state_ensure = self.python_mod.base_addr + self.python_mod.exports['PyGILState_Ensure']
         py_run_simple_string = self.python_mod.base_addr + self.python_mod.exports['PyRun_SimpleString']
+        py_eval_save_thread = self.python_mod.base_addr + self.python_mod.exports['PyEval_SaveThread']
         
-        addr = self._process.allocate(len(py_code))
-        self._process.write(addr, py_code)
+        gil_ensure_thread = self._process.start_thread(py_gil_state_ensure)
+        self._process.join_thread(gil_ensure_thread)
         
-        thread = self._process.start_thread(py_run_simple_string, addr)
-        self._process.join_thread(thread)
+        pycode_addr = self._process.allocate(len(py_code))
+        self._process.write(pycode_addr, py_code)
+        exec_thread = self._process.start_thread(py_run_simple_string, pycode_addr)
+        self._process.join_thread(exec_thread)
+        self._process.free(pycode_addr)
+        
+        savethread_thread = self._process.start_thread(py_eval_save_thread)
+        self._process.join_thread(savethread_thread)
     
     
     def finalize(self) -> None:
-        """Undo all initializations of the Python interpreter in the target process"""
+        """Undo all initializations of the Python interpreter in the target process
+        This method calls Py_FinalizeEx
+        """
         py_finalize_ex = self.python_mod.base_addr + self.python_mod.exports['Py_FinalizeEx']
         
         thread = self._process.start_thread(py_finalize_ex)
@@ -149,4 +168,6 @@ class PythonLib:
         self.exec(py_code)
         
         addr = self._process.read(ret_addr, 8)
+        self._process.free(ret_addr)
+        
         return int.from_bytes(addr, 'little')
