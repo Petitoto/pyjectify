c0a24882977b8d4dfaacbc30cbe208e05bc646e5
Petitoto <Petitoto@users.noreply.github.com>
2023-12-02 17:55:44 +0100

    apisetschema: use VirtualSize instead of SizeOfRawData

--- pyjectify/windows/utils/apisetschema.py
+++ pyjectify/windows/utils/apisetschema.py
@@ -20,7 +20,7 @@ class ApiSetSchema:
         
         for section in apisetschema.sections_header:
             if bytes(section.Name).strip(b'\x00') == b'.apiset':
-                self._data = apisetschema.raw[section.VirtualAddress:section.VirtualAddress+section.SizeOfRawData]
+                self._data = apisetschema.raw[section.VirtualAddress:section.VirtualAddress+section.Misc.VirtualSize]
                 break
         
         if not self._data:
