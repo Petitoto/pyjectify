cddc6ed22a7af13f4d6fc5251fdb4779af409b3e
Petitoto <Petitoto@users.noreply.github.com>
2024-05-24 18:07:38 +0200

    Inject: add "call_entry_point" parameter to memory_loader

--- pyjectify/windows/modules/inject.py
+++ pyjectify/windows/modules/inject.py
@@ -48,13 +48,14 @@ class Inject:
             return self._process.get_module(libpath)
     
     
-    def memory_loader(self, module: PE, prefer_base_addr: bool = True, copy_headers: bool = True) -> PE:
+    def memory_loader(self, module: PE, prefer_base_addr: bool = True, copy_headers: bool = True, call_entry_point: bool = True) -> PE:
         """Fully map a library from memory in the target process, resolving imports and ApiSet
         
         Args:
-            module: a PyJectify's PE object representinf the library to load in the target process
+            module: a PyJectify's PE object representing the library to load in the target process
             prefer_base_addr: specify whether to load the module at the PE's default base address
             copy_headers: specify whether to copy the PE headers to the target process memory
+            call_entry_point: specify whether to call the entry point with DLL_PROCESS_ATTACH flag
         
         Returns:
             A PyJectify's PE object representing the library loaded in the target process
@@ -101,19 +102,20 @@ class Inject:
         for section_addr, section_size, protect in module.sections:
             self._process.protect(module.base_addr + section_addr, section_size, protect)
         
-        entry_point = module.base_addr + module.nt_header.OptionalHeader.AddressOfEntryPoint
-        
-        if self._process.x86:
-            entry_call = _entry_call_x86 % (entry_point.to_bytes(4, 'little'), module.base_addr.to_bytes(4, 'little'))
-        else:
-            entry_call = _entry_call_x64 % (entry_point.to_bytes(8, 'little'), module.base_addr.to_bytes(8, 'little'))
-        
-        entry_call_addr = self._process.allocate(len(entry_call), protect=PAGE_READWRITE)
-        self._process.write(entry_call_addr, entry_call)
-        self._process.protect(entry_call_addr, len(entry_call), PAGE_EXECUTE_READ)
-        
-        thread = self._process.start_thread(entry_call_addr)
-        self._process.join_thread(thread)
-        self._process.free(entry_call_addr)
+        if call_entry_point:
+            entry_point = module.base_addr + module.nt_header.OptionalHeader.AddressOfEntryPoint
+            
+            if self._process.x86:
+                entry_call = _entry_call_x86 % (entry_point.to_bytes(4, 'little'), module.base_addr.to_bytes(4, 'little'))
+            else:
+                entry_call = _entry_call_x64 % (entry_point.to_bytes(8, 'little'), module.base_addr.to_bytes(8, 'little'))
+            
+            entry_call_addr = self._process.allocate(len(entry_call), protect=PAGE_READWRITE)
+            self._process.write(entry_call_addr, entry_call)
+            self._process.protect(entry_call_addr, len(entry_call), PAGE_EXECUTE_READ)
+            
+            thread = self._process.start_thread(entry_call_addr)
+            self._process.join_thread(thread)
+            self._process.free(entry_call_addr)
         
         return module
