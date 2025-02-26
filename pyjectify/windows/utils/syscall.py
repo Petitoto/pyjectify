Petitoto/pyjectify

commit 100a09bd91358322c1fbd8b5236ad4b0fa922f85

Add vscode settings, mypy, pylance

pyjectify/windows/utils/syscall.py
@@ -1,20 +1,20 @@
 import ctypes.util
-from typing import Dict, Callable
+from typing import Callable, Dict
 
 from pyjectify.windows.core.defines import *
-from pyjectify.windows.core.process import ProcessHandle
 from pyjectify.windows.core.pe import PE
+from pyjectify.windows.core.process import ProcessHandle
 
 _syscode_signatures = [
     b'\xb8',                 # mov    eax, syscode (x86)
     b'\x4c\x8b\xd1\xb8'      # mov    r10,rcx ; mov    rax, syscode (x64)
-    ]
+]
 
 _syscall_x86 = b'\x5a'                     # pop    edx
 _syscall_x86 += b'\x58'                    # pop    eax
 _syscall_x86 += b'\x50'                    # push   eax
 _syscall_x86 += b'\x52'                    # push   edx
-_syscall_x86 += b'\x89\xe2'                # mov    edx,esp
+_syscall_x86 += b'\x89\xe2'                # mov    edx, esp
 _syscall_x86 += b'\x0f\x34'                # sysenter
 _syscall_x86 += b'\xc3'                    # ret
 
@@ -38,8 +38,8 @@ class Syscall:
     This util does not support WOW64.
     """
     
-    syscalltable: dict #: Dict of syscall -> syscode, store the retrieved syscall codes
-    syscall: ctypes.CFUNCTYPE #: syscall wrapper, first argument is the syscode and other arguments are the sycall arguments
+    syscalltable: dict  #: Dict of syscall -> syscode, store the retrieved syscall codes
+    syscall: Callable   #: syscall wrapper, first argument is the syscode and other arguments are the sycall arguments
     
     def __init__(self, syscalltable: Dict[str, str] = {}) -> None:
         self.syscalltable = syscalltable
@@ -69,7 +69,10 @@ class Syscall:
         data = b''
         
         if from_disk:
-            ntdll_pe = PE(open(ctypes.util.find_library('ntdll.dll'), 'rb').read())
+            ntdll_path = ctypes.util.find_library('ntdll.dll')
+            if not ntdll_path:
+                raise AssertionError('ntdll.dll not found')
+            ntdll_pe = PE(open(ntdll_path, 'rb').read())
             addr = ntdll_pe.exports[syscall]
             data = ntdll_pe.raw[addr:addr+8]
         else:
@@ -105,15 +108,10 @@ class Syscall:
     
     
     def __getattr__(self, syscall: str) -> Callable:
-        if not syscall in self.syscalltable:
+        if syscall not in self.syscalltable:
             return ntdll.__getattribute__(syscall)
         
         syscode = self.syscalltable[syscall]
-        
-        try:
-            self.syscall.argtypes = (DWORD,) + ntdll.__getattribute__(syscall).argtypes
-        except:
-            pass
         
         def _call(*args):
             return self.syscall(syscode, *args)

pyjectify/__init__.py
@@ -1,9 +1,10 @@
 import platform as platform
 
-system = platform.system() #: Current operating system
+system = platform.system()  #: Current operating system
 
 if system == 'Windows':
     from pyjectify.windows import *
-    __all__ = ['PyJectifyWin', 'byName', 'defines', 'PE', 'ApiSetSchema', 'Syscall', 'x86', 'wow64', 'windowsx86', 'system']
+    __all__ = ['PyJectifyWin', 'byName', 'defines', 'PE', 'ApiSetSchema',
+               'Syscall', 'x86', 'wow64', 'windowsx86', 'system']
 else:
     print('Warning: PyJectify does not support your system!')

pyjectify/windows/__init__.py
@@ -16,30 +16,30 @@ from pyjectify.windows.utils.syscall import Syscall
 __all__ = ['PyJectifyWin', 'byName', 'defines', 'PE', 'ApiSetSchema', 'Syscall', 'x86', 'wow64', 'windowsx86']
 
 
-x86: bool #: Specify if PyJectify process runs in 32-bit mode
-wow64: bool #: Specify if PyJectify process is a wow64 process
-windowsx86: bool #: Specify if Windows is 32-bit
+x86: bool         #: Specify if PyJectify process runs in 32-bit mode
+wow64: bool       #: Specify if PyJectify process is a wow64 process
+windowsx86: bool  #: Specify if Windows is 32-bit
 
 x86 = ctypes.sizeof(defines.SIZE_T) == 4
-wow64 = defines.BOOL()
-if not defines.kernel32.IsWow64Process(-1, ctypes.byref(wow64)):
+is_wow64 = defines.BOOL()
+if not defines.kernel32.IsWow64Process(-1, ctypes.byref(is_wow64)):
     raise _WinAPIError('IsWow64Process - %s' % (defines.kernel32.GetLastError()))
-wow64 = wow64.value > 0
+wow64 = is_wow64.value > 0
 windowsx86 = x86 and not wow64
 
 
 class PyJectifyWin:
     """This class represents the main Pyjectify object for Windows and gives access to all modules"""
-    
+
     process: _ProcessHandle  #: Target Process
-    memscan: _MemScan #: MemScan module initialized for the target process
-    hook: _Hook #: Hook module initialized for the target process
-    inject: _Inject #: Inject module initialized for the target process
-    pythonlib: _PythonLib #: PythonLib module initialized for the target process
-    
+    memscan: _MemScan        #: MemScan module initialized for the target process
+    hook: _Hook              #: Hook module initialized for the target process
+    inject: _Inject          #: Inject module initialized for the target process
+    pythonlib: _PythonLib    #: PythonLib module initialized for the target process
+
     def __init__(self, pid: int) -> None:
         self.process = _ProcessHandle(pid)
-        
+
         self.memscan = _MemScan(self.process)
         self.inject = _Inject(self.process)
         self.hook = _Hook(self.process)
@@ -48,10 +48,10 @@ class PyJectifyWin:
 
 def byName(process: str) -> list[PyJectifyWin]:
     """Return a list of PyjectifyWin objects based on a process name
-    
+
     Args:
         process: Process name
-    
+
     Returns:
         List of PyJectifyWin objects associated with the process name
     """

pyjectify/windows/core/defines.py
@@ -43,6 +43,8 @@ LPCWSTR = LPWSTR = ctypes.c_wchar_p
 LPCSTR = LPSTR = ctypes.c_char_p
 LPCVOID = LPVOID = ctypes.c_void_p
 
+WPARAM: type[ctypes.c_ulong] | type[ctypes.c_ulonglong]
+LPARAM: type[ctypes.c_long] | type[ctypes.c_longlong]
 if ctypes.sizeof(ctypes.c_long) == ctypes.sizeof(ctypes.c_void_p):
     WPARAM = ctypes.c_ulong
     LPARAM = ctypes.c_long
@@ -426,14 +428,14 @@ class IMAGE_NT_HEADERS64(ctypes.Structure):
     ]
 
 class IMAGE_SECTION_HEADER(ctypes.Structure):
-    class Misc(ctypes.Union):
+    class MiscUnion(ctypes.Union):
         _fields_ = [
             ('PhysicalAddress', DWORD),
             ('VirtualSize', DWORD),
         ]
     _fields_ = [
         ('Name', BYTE * 8),
-        ('Misc', Misc),
+        ('Misc', MiscUnion),
         ('VirtualAddress', DWORD),
         ('SizeOfRawData', DWORD),
         ('PointerToRawData', DWORD),

pyjectify/windows/core/pe.py
@@ -1,21 +1,25 @@
 from io import BytesIO
+from typing import TypeVar
 
 from pyjectify.windows.core.defines import *
 
 
+STRUCT = TypeVar('STRUCT', bound=ctypes.Structure)
+
+
 class PE:
     """This class represents a PE and provides methods to parse it"""
-    
-    raw: bytes #: Raw bytes of the PE, mapped to memory
-    base_addr: int #: Base address of the PE
-    dos_header: IMAGE_DOS_HEADER #: DOS headers
-    nt_header: IMAGE_NT_HEADERS32 #: NT headers
-    x86: bool #: Specify if the PE is a 32-bit PE
-    sections_header: list[IMAGE_SECTION_HEADER] #: PE sections headers
-    sections: list #: PE sections, list of (VirtualAddress, VirtualSize, PageProtection) tuple
-    exports: dict #: PE exports, dict of function_name -> function_address ; addresses are relative to the module base address
-    imports: dict #: PE imports, dict of library_name -> [(function_name, function_address)...]
-    
+
+    raw: bytes                                         #: Raw bytes of the PE, mapped to memory
+    base_addr: int                                     #: Base address of the PE
+    dos_header: IMAGE_DOS_HEADER                       #: DOS headers
+    nt_header: IMAGE_NT_HEADERS32 | IMAGE_NT_HEADERS64 #: NT headers
+    x86: bool                                          #: Specify if the PE is a 32-bit PE
+    sections_header: list[IMAGE_SECTION_HEADER]        #: PE sections headers
+    sections: list                                     #: PE sections, list of (VirtualAddress, VirtualSize, PageProtection) tuple
+    exports: dict                                      #: PE exports, dict of function_name -> function_address ; addresses are relative to the module base address
+    imports: dict                                      #: PE imports, dict of library_name -> [(function_name, function_address)...]
+
     def __init__(self, raw: bytes, base_addr: int = 0, headers_only: bool = False) -> None:
         self.raw = raw
         self.base_addr = base_addr
@@ -23,15 +27,15 @@ class PE:
         self.sections = []
         self.exports = {}
         self.imports = {}
-        
+
         self.dos_header = self._fill_struct(IMAGE_DOS_HEADER, 0)
         if bytes(WORD(self.dos_header.e_magic)) != b'MZ':
             raise InvalidPEHeader('Invalid MZ signature - %s' % (self.dos_header.e_magic))
-        
+
         self.nt_header = self._fill_struct(IMAGE_NT_HEADERS32, self.dos_header.e_lfanew)
         if bytes(DWORD(self.nt_header.Signature)) != b'PE\x00\x00':
             raise InvalidPEHeader('Invalid PE signature - %s' % (self.nt_header.Signature))
-        
+
         if self.nt_header.OptionalHeader.Magic == IMAGE_NT_OPTIONAL_HDR32_MAGIC:
             self.x86 = True
             self.nt_header = self._fill_struct(IMAGE_NT_HEADERS32, self.dos_header.e_lfanew)
@@ -42,18 +46,18 @@ class PE:
             section_addr = self.dos_header.e_lfanew + ctypes.sizeof(IMAGE_NT_HEADERS64)
         else:
             raise InvalidPEHeader('Invalid Optional Magic Header - %s' % (self.nt_header.OptionalHeader.Magic))
-        
+
         if not self.base_addr:
             self.base_addr = self.nt_header.OptionalHeader.ImageBase
-        
+
         for i in range(self.nt_header.FileHeader.NumberOfSections):
             section_header = self._fill_struct(IMAGE_SECTION_HEADER, section_addr + i*ctypes.sizeof(IMAGE_SECTION_HEADER))
             self.sections_header.append(section_header)
-            
+
             protectR = section_header.Characteristics & IMAGE_SCN_MEM_READ
             protectW = section_header.Characteristics & IMAGE_SCN_MEM_WRITE
             protectX = section_header.Characteristics & IMAGE_SCN_MEM_EXECUTE
-            
+
             if not protectR and not protectW and protectX:
                 protect = PAGE_EXECUTE
             elif protectR and not protectW and protectX:
@@ -70,85 +74,86 @@ class PE:
                 protect = PAGE_READONLY
             elif protectR and protectW and not protectX:
                 protect = PAGE_READWRITE
-            
+
             self.sections.append((section_header.VirtualAddress, section_header.Misc.VirtualSize, protect))
-        
+
         if not headers_only:
             self._map_to_memory()
             self._parse_imports()
             self._parse_exports()
-    
-    
-    def _fill_struct(self, struct: ctypes.Structure, addr: ctypes.Structure) -> ctypes.Structure:
+
+
+    def _fill_struct(self, struct: type[STRUCT], addr: int) -> STRUCT:
         length = ctypes.sizeof(struct)
         buf = self._read_raw(addr, length)
         return struct.from_buffer_copy(buf)
-    
-    
+
+
     def _read_raw(self, addr: int, length: int) -> bytes:
         return self.raw[addr:addr + length]
-    
-    
+
+
     def _read_int(self, addr: int, length: int) -> int:
         return int.from_bytes(self.raw[addr:addr + length], byteorder='little')
-    
-    
+
+
     def _read_str(self, addr: int, length: int = 1024) -> str:
         data = self.raw[addr:addr+length]
         return data[:data.find(0)].decode()
-    
-    
-    def _is_mapped(self) -> None:
+
+
+    def _is_mapped(self) -> bool:
         if self.x86:
             section_addr = self.dos_header.e_lfanew + ctypes.sizeof(IMAGE_NT_HEADERS32)
         else:
             section_addr = self.dos_header.e_lfanew + ctypes.sizeof(IMAGE_NT_HEADERS64)
-        
+
         virtual_end = section_addr + len(self.sections_header) * ctypes.sizeof(IMAGE_SECTION_HEADER)
         for section_header in self.sections_header:
             data = self.raw[virtual_end:section_header.VirtualAddress]
             if data != b'\x00'*len(data):
                 break
             virtual_end = section_header.VirtualAddress + section_header.Misc.VirtualSize
-        
+
         return data == b'\x00'*len(data)
-    
-    
+
+
     def _map_to_memory(self) -> None:
         if not self.sections_header:
             raise InvalidPEHeader('No sections found')
-        
+
         if self._is_mapped():
             return
-        
+
         size = self.nt_header.OptionalHeader.SizeOfImage
         raw = BytesIO(b'\x00'*size)
-        
+
         raw.write(self._read_raw(0, self.nt_header.OptionalHeader.SizeOfHeaders))
-        
+
         for section_header in self.sections_header:
             raw.seek(section_header.VirtualAddress)
             raw.write(self._read_raw(section_header.PointerToRawData, section_header.SizeOfRawData))
-        
+
         raw.seek(0)
         self.raw = raw.read()
-    
-    
+
+
     def _parse_imports(self) -> None:
         data_directory = self.nt_header.OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT]
-        
+
         if not data_directory.VirtualAddress:
             return
-        
+
         offset = 0
         import_dir = self._fill_struct(IMAGE_IMPORT_DESCRIPTOR, data_directory.VirtualAddress)
-        
+
         while import_dir.Characteristics:
             dll_name = self._read_str(import_dir.Name)
             self.imports[dll_name] = []
-            
+
             i = 0
             while True:
+                othunk: IMAGE_THUNK_DATA32 | IMAGE_THUNK_DATA64
                 if self.x86:
                     thunk_addr = import_dir.FirstThunk + i*ctypes.sizeof(IMAGE_THUNK_DATA32)
                     othunk_addr = import_dir._u.OriginalFirstThunk + i*ctypes.sizeof(IMAGE_THUNK_DATA32)
@@ -157,10 +162,10 @@ class PE:
                     thunk_addr = import_dir.FirstThunk + i*ctypes.sizeof(IMAGE_THUNK_DATA64)
                     othunk_addr = import_dir.OriginalFirstThunk + i*ctypes.sizeof(IMAGE_THUNK_DATA64)
                     othunk = self._fill_struct(IMAGE_THUNK_DATA64, othunk_addr)
-                
+
                 if not othunk.u1.AddressOfData:
                     break
-                
+
                 if self.x86 and IMAGE_SNAP_BY_ORDINAL32(othunk.u1.Ordinal):
                     self.imports[dll_name].append((IMAGE_ORDINAL32(othunk.u1.Ordinal), thunk_addr))
                     i += 1
@@ -169,112 +174,114 @@ class PE:
                     self.imports[dll_name].append((IMAGE_ORDINAL64(othunk.u1.Ordinal), thunk_addr))
                     i += 1
                     continue
-                
+
                 import_by_name = self._fill_struct(IMAGE_IMPORT_BY_NAME, othunk.u1.AddressOfData)
                 self.imports[dll_name].append((import_by_name.Name.decode(), thunk_addr))
-                
+
                 i += 1
-            
+
             offset += ctypes.sizeof(IMAGE_IMPORT_DESCRIPTOR)
             import_dir = self._fill_struct(IMAGE_IMPORT_DESCRIPTOR, data_directory.VirtualAddress + offset)
-    
-    
+
+
     def _parse_exports(self) -> None:
         data_directory = self.nt_header.OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT]
-        
+
         if not data_directory.VirtualAddress:
             return
-        
+
         export_directory = self._fill_struct(IMAGE_EXPORT_DIRECTORY, data_directory.VirtualAddress)
-        
+
         if export_directory.AddressOfFunctions:
             for i in range(export_directory.NumberOfFunctions):
                 function_addr = self._read_int(export_directory.AddressOfFunctions + 4*i, 4)
                 self.exports[i] = function_addr
-            
+
             for i in range(export_directory.NumberOfNames):
                 ord = self._read_int(export_directory.AddressOfNameOrdinals + 2*i, 2)
                 function_addr = self.exports[ord]
                 name_addr = self._read_int(export_directory.AddressOfNames + 4*i, 4)
                 name = self._read_str(name_addr)
                 self.exports[name] = function_addr
-    
-    
+
+
     def forwarded_export(self, name: str) -> str:
         """Resolve a forwarded export
-        
+
         Args:
             name: the name of the forwarded export
-        
+
         Returns:
             The name of the resolved forwarded export
         """
         addr = self.exports[name]
-        
+
         begin_exports = self.nt_header.OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].VirtualAddress
         end_exports = begin_exports + self.nt_header.OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].Size
-        
+
         if begin_exports <= addr < end_exports:
             return self._read_str(addr)
-    
-    
+
+        return ""
+
+
     def change_base(self, base_addr: int) -> None:
         """Change PE base address and perform base relocation
-        
+
         Args:
             base_addr: new base address of the PE
         """
         delta = base_addr - self.base_addr
-        
+
         if delta:
             raw = BytesIO(self.raw)
             relocations = self.nt_header.OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_BASERELOC]
-            
+
             if relocations.Size:
                 offset = 0
                 relocation = self._fill_struct(BASE_RELOCATION_BLOCK, relocations.VirtualAddress)
-                
+
                 while relocation.PageAddress:
                     offset += ctypes.sizeof(BASE_RELOCATION_BLOCK)
-                    
+
                     entries_count = (relocation.BlockSize - ctypes.sizeof(BASE_RELOCATION_BLOCK)) // ctypes.sizeof(BASE_RELOCATION_ENTRY)
-                    
+
                     for _ in range(entries_count):
                         entry = self._fill_struct(BASE_RELOCATION_ENTRY, relocations.VirtualAddress + offset)
                         offset += ctypes.sizeof(BASE_RELOCATION_ENTRY)
-                        
+
                         relocation_addr = relocation.PageAddress + entry.Offset
                         raw.seek(relocation_addr)
-                        
+
                         if entry.Type == IMAGE_REL_BASED_ABSOLUTE:
                             continue
-                        
+
                         elif entry.Type == IMAGE_REL_BASED_HIGH:
                             address = self._read_int(relocation_addr, 2) + HIWORD(delta)
                             raw.write(address.to_bytes(2, byteorder='little'))
-                        
+
                         elif entry.Type == IMAGE_REL_BASED_LOW:
                             address = self._read_int(relocation_addr, 2) + LOWORD(delta)
                             raw.write(address.to_bytes(2, byteorder='little'))
-                        
+
                         elif entry.Type == IMAGE_REL_BASED_HIGHLOW:
                             address = self._read_int(relocation_addr, 4) + delta
                             raw.write(address.to_bytes(4, byteorder='little'))
-                        
+
                         elif entry.Type == IMAGE_REL_BASED_HIGHADJ:
                             address = self._read_int(relocation_addr, 2) + HIWORD(delta)
                             raw.write(address.to_bytes(2, byteorder='little'))
                             raw.write(LOWORD(delta).to_bytes(2, byteorder='little'))
-                        
+
                         elif entry.Type == IMAGE_REL_BASED_DIR64:
                             address = self._read_int(relocation_addr, 8) + delta
                             raw.write(address.to_bytes(8, byteorder='little'))
-                        
+
                         else:
                             raise InvalidPEHeader('Unknown relocation entry type - %s' % entry.Type)
-                        
+
                     relocation = self._fill_struct(BASE_RELOCATION_BLOCK, relocations.VirtualAddress + offset)
-            
+
             if self.x86:
                 self.nt_header.OptionalHeader.ImageBase = DWORD(base_addr)
                 raw.seek(self.dos_header.e_lfanew + IMAGE_NT_HEADERS32.OptionalHeader.offset + IMAGE_OPTIONAL_HEADER32.ImageBase.offset)
@@ -283,30 +290,31 @@ class PE:
                 self.nt_header.OptionalHeader.ImageBase = ULONGLONG(base_addr)
                 raw.seek(self.dos_header.e_lfanew + IMAGE_NT_HEADERS64.OptionalHeader.offset + IMAGE_OPTIONAL_HEADER64.ImageBase.offset)
                 raw.write(base_addr.to_bytes(8, byteorder='little'))
-            
+
             raw.seek(0)
             self.raw = raw.read()
             self.base_addr = base_addr
-    
-    
+
+
     def patch_import(self, thunk_addr: int, address: int) -> None:
         """Patch PE imports
-        
+
         Args:
             thunk_addr: address of the thunk data of the import
             address: new function address for the import
         """
         raw = BytesIO(self.raw)
-        
+
+        thunk: IMAGE_THUNK_DATA32 | IMAGE_THUNK_DATA64
         if self.x86:
             thunk = self._fill_struct(IMAGE_THUNK_DATA32, thunk_addr)
         else:
             thunk = self._fill_struct(IMAGE_THUNK_DATA64, thunk_addr)
-        
+
         thunk.u1.Function = address
         raw.seek(thunk_addr)
         raw.write(thunk)
-        
+
         raw.seek(0)
         self.raw = raw.read()
 

pyjectify/windows/modules/hook.py
@@ -1,51 +1,51 @@
-from pyjectify.windows.core.process import ProcessHandle
 from pyjectify.windows.core.defines import *
+from pyjectify.windows.core.process import ProcessHandle
 
-_jmp_x86 = b'\xb8%s'     # mov    eax, address
-_jmp_x86 += b'\xff\xe0'  # jmp    eax
+_jmp_x86 = b'\xb8%s'      # mov    eax, address
+_jmp_x86 += b'\xff\xe0'   # jmp    eax
 
-_jmp_x64 = b'\x48\xb8%s' # movabs rax, address
+_jmp_x64 = b'\x48\xb8%s'  # movabs rax, address
 _jmp_x64 += b'\xff\xe0'   # jmp    rax
 
 
 class Hook:
     """This class provides methods to hook functions of the target process"""
-    
+
     def __init__(self, process: ProcessHandle) -> None:
         self._process = process
-    
-    
+
+
     def trampoline(self, target: int, size: int) -> int:
         """Set up a trampoline to the target function with specified size
-        
+
         Args:
             target: the target address of the function the trampoline will jump to
             size: the size of the bytes of the original function to copy in the trampoline before jumping
-        
+
         Returns:
             The address of the trampoline
-        
+
         Warning:
             For classic inline hooks, the size must be superior to the length of the jump code (7 bytes for 32-bits and 12 bytes for 64-bytes) and must correspond to the size of the first complete instructions of the original function
         """
         code = self._process.read(target, size)
-        
+
         target += size
         if self._process.x86:
             jmp = _jmp_x86 % target.to_bytes(4, 'little')
         else:
             jmp = _jmp_x64 % target.to_bytes(8, 'little')
         code += jmp
-        
+
         trampoline_addr = self._process.allocate(size)
         self._process.write(trampoline_addr, code)
         self._process.protect(trampoline_addr, size, PAGE_EXECUTE_READ)
         return trampoline_addr
-    
-    
+
+
     def inline(self, target: int, hook: int) -> None:
         """Set up an inline hook to the target function
-        
+
         Args:
             target: address of the function to hook
             hook: address of the hooking function
@@ -54,7 +54,7 @@ class Hook:
             jmp = _jmp_x86 % hook.to_bytes(4, 'little')
         else:
             jmp = _jmp_x64 % hook.to_bytes(8, 'little')
-        
+
         old_protect = self._process.protect(target, len(jmp), PAGE_READWRITE)
         self._process.write(target, jmp)
         self._process.protect(target, len(jmp), old_protect)

commit 2f018f2a04ce11261c8cb02c8ae9046437526e29

fix: set syscall.argtypes before calling syscall wrapper (bug introduced in 100a09b)

pyjectify/windows/utils/syscall.py
@@ -134,6 +134,11 @@ class Syscall:
 
         syscode = self._syscalltable[syscall]
 
+        try:
+            self._syscall.argtypes = (DWORD,) + ntdll.__getattribute__(syscall).argtypes
+        except:
+            pass
+
         def _call(*args: Any):
             return self._syscall(syscode, *args)
 